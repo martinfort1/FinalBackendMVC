0bc9bbb8070011d5005bcfb23be6c7a0073e95be
Martin <martinfort13@gmail.com>
2023-11-24 03:27:19 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<DataContext>(options =>
 options.UseSqlServer(builder.Configuration
 .GetConnectionString("DefaultConnection")));
+
 IMapper mapper = MapperConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
